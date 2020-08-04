61532c303ce5d43a6d22d0372b533a7dd0a6ca42
Lee <leegould119@gmail.com>
2020-08-04 13:19:24 +0200

    updates...

--- backend/routes/test-routes.js
+++ backend/routes/test-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 //  ROUTER LEVEL MIDDLEWARE
 const timeLogger = require('./timeLogger');
 const verify = require('./verifyToken');
+
 // parameter bases endpoint
 router.get('/parameters', timeLogger, (req, res, next) => {
   const userId = req.param('userId');
@@ -16,4 +17,5 @@ router.get('/headers', timeLogger, verify, (req, res, next) => {
   const Auth = req.header('auth-token');
   res.send(Auth);
 });
+
 module.exports = router;
